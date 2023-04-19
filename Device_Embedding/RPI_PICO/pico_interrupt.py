13543e55b664fb45f8cdc643e05c31392d92a8fc
minkyukang707 <87360858+minkyukang707@users.noreply.github.com>
2023-04-19 21:33:53 +0900

    Add files via upload

---
+++ Device_Embedding/RPI_PICO/pico_interrupt.py
@@ -0,0 +1,10 @@
+# https://electrocredible.com/raspberry-pi-pico-external-interrupts-button-micropython/
+
+from machine import Pin
+counter=0
+pin = Pin(5, Pin.IN, Pin.PULL_UP)
+while True:
+    if pin.value()==0:
+        print("Button Pressed")
+        counter+=1
+        print("Count={}".format(counter))
